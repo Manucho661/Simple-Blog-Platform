c8f0f4d75b83510a4adcb4d3af95f8527009f4ad
Manucho661 <emmanuelwanyonyi49@gmail.com>
2024-01-08 22:49:12 +0300

    22,8,1,24

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,16 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>SIMPLE BLOG POST, WOW</h1>
+   <div style="position: relative; left:30%; color:blue">   <h1>SOFTECH TECHNOLOGIES</h1> </div>
+    <p style="margin-left:10px">Welcome to SOFTECH technologies, a platform enables you to create and publish beautiful blog posts</p>
+    <form action="/register" method="POST">
+        @csrf
+        <label for="email">Enter email</label>
+<input type="email" name="email" placeholder="emmanu@gmail.com">
+<label for="password">Enter password</label>
+<input type="password" name="password" >
+<button>Register</button>
+
+    </form>
 </body>
 </html>
